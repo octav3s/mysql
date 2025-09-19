26a8dda82a73d6d1e9d57739812dbb6f287ab7f2
aditya prasad <adityaprasad85470@gmail.com>
2025-09-19 15:47:34 +0530

    student info

--- studentinfo.sql
+++ studentinfo.sql
@@ -2,7 +2,6 @@ CREATE TABLE student_info (id INT(20) PRIMARY KEY, name VARCHAR(20), age INT(10)
 INSERT INTO student_info VALUES(1,'Aromal', 21, 'CSE', 8);
 INSERT INTO student_info VALUES(2,'Abdu', 21, 'CSE', 10);
 INSERT INTO student_info VALUES(3,'Anandu', 21, 'CSE', 7);
-INSERT INTO student_info VALUES(4,'Adithyan', 22, 'ECE', 8);
 
 
 SELECT * FROM `student_info` WHERE age>20;
